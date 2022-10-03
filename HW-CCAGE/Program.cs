fdd6fa3ca5815cc1a3d94c70dec7e647eaf6dceb
roler <roler@DESKTOP-IFLVOR0>
2022-10-03 18:38:24 +0300

    Добавьте файлы проекта.

---
+++ HW-CCAGE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_CCAGE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Александр Блок");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Ночь, улица, фонарь, аптека");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ночь, улица, фонарь, аптека,\r\nБессмысленный и тусклый свет.\r\nЖиви еще хоть четверть века —\r\nВсё будет так. Исхода нет.\r\n\r\nУмрешь — начнешь опять сначала\r\nИ повторится всё, как встарь:\r\nНочь, ледяная рябь канала,\r\nАптека, улица, фонарь.");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW-CCAGE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HW-CCAGE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW-CCAGE")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bc001bcd-8b7b-4131-82d7-3dc82e2633f3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
